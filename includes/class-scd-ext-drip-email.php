f85a5d934004510075952ff37568226189a5d95d
Dwain Maralack <dwain.maralack@gmail.com>
2014-09-08 23:17:40 +1000

    add the new email class file

---
+++ includes/class-scd-ext-drip-email.php
@@ -0,0 +1,23 @@
+<?php  
+//security first
+if ( ! defined( 'ABSPATH' ) ) exit;
+/*
+ * Sensei Content Drip ( scd ) Email functionality class
+ *
+ * This class handles all of the functionality for the plugins email functionality.
+ *
+ * @package WordPress
+ * @subpackage Sensei Content Drip
+ * @category Core
+ * @author WooThemes
+ * @since 1.0.0
+ *
+ * TABLE OF CONTENTS
+ *
+ * - __construct()
+ *
+ */
+
+class Scd_Ext_drip_email {
+
+}// end Scd_Ext_drip_email
