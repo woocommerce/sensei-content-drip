c2848a2df59a7ad8c7db0d2cfe76cd478f11cb16
Dwain Maralack <dwain.maralack@gmail.com>
2014-09-09 16:57:10 +1000

    adding a new utilities class

---
+++ includes/class-scd-ext-utilities.php
@@ -0,0 +1,22 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Sensei Content Drip Utilities Class
+ *
+ * Common functions used by the Content drip extension
+ *
+ * @package WordPress
+ * @subpackage Sensei Content Drip
+ * @category Utilities
+ * @author WooThemes
+ * @since 1.0.0
+ *
+ * TABLE OF CONTENTS
+ *
+ *
+ */
+class Sensei_Scd_Extension_Utils {
+
+
+} // end class Sensei_Scd_Extension_Utils
