a9c4c61d4751ecfc43b043ac2ff58917c0023d2b
Dwain Maralack <dwain.maralack@gmail.com>
2014-09-11 00:56:08 +1000

    #4 completed the dynamic daily email drip lessons check

--- includes/class-scd-ext-drip-email.php
+++ includes/class-scd-ext-drip-email.php
@@ -17,7 +17,10 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * - __construct()
  * - daily_drip_lesson_email_run()
  * - get_todays_dripping_users_lessons_list()
- * - 
+ * - get_dynamic_user_courses_lessons_list
+ * - generate_lesson_and_pre_requisite_list()
+ * - get_courses_lessons()
+ * - get_dynamic_users_by_courses
  */
 
 class Scd_Ext_drip_email {
@@ -57,8 +60,9 @@ class Scd_Ext_drip_email {
 	public function get_todays_dripping_users_courses_lessons_list(){
 
 		// get each user list by the different drip types
-		$dynamic_dripping_users_lessons_list = $this->get_dynamic_user_courses_lessons_list();
-		//$absolute_dripping_users_lessons_list = $this->get_absolute_user_courses_lessons_list();
+		$dynamic_dripping_users_lessons_list = $this->get_dynamic_user_courses_lessons_dripping_today_list();
+		
+		//$absolute_dripping_users_lessons_list = $this->get_absolute_user_courses_lessons_dripping_today_list();
 
 		// both are empty return empty array and exit
 		//if( empty( $dynamic_dripping_users_lessons_list ) && empty( $dynamic_dripping_users_lessons_list ) ){
@@ -81,19 +85,18 @@ class Scd_Ext_drip_email {
 	*
 	* @return array $lessons
 	*/
-	public function get_dynamic_user_courses_lessons_list(){
+	public function get_dynamic_user_courses_lessons_dripping_today_list(){
 		global $woo_sensei_content_drip;
 
-		$dynamic_users_courses_lessons =  array();
+		$dynamic_users_lessons_courses_dripping_today =  array();
 
 		// get all lesson id's with dripy type == 'dynamic'
-		//var_dump( Sensei_Content_Drip()); die;
 		$all_dynamic_lessons = $woo_sensei_content_drip->utils->get_dripping_lessons_by_type('dynamic');
 
 		// return the emptiness
 		if( empty( $all_dynamic_lessons ) ){
 			// exit
-			return $lessons;
+			return $dynamic_users_lessons_courses_dripping_today;
 		}
 
 		// for all the dynamic lessons get their pre_requisites 
@@ -101,38 +104,19 @@ class Scd_Ext_drip_email {
 
 		if(empty( $lessons_and_pre_requisite ) ){
 			// exit
-			return $lessons;
+			return $dynamic_users_lessons_courses_dripping_today;
 		}
 
 		//get the lessons courses 
 		$courses_lessons_pre_requisites = $this->get_courses_lessons( $lessons_and_pre_requisite  );
 
-		if(empty( $courses_lessons_pre_requisites ) ){
-			// exit
-			return $lessons;
-		}
-
 		// create an array of users > courses > lessons
 		$dynamic_users_courses_lessons = $this->get_dynamic_users_by_courses( $courses_lessons_pre_requisites );
-		
 
+		//check all course to see if they are drippping today
+		$dynamic_users_lessons_courses_dripping_today = $this->get_dripping_today( $dynamic_users_courses_lessons );
 
-		// go through each user and their lessons to see if the pre-requisite lesson was completed
-		// then the lesson should be remove from the user
-	/*	foreach ($dynamic_users_courses_lessons as $user_id ) {
-			foreach ($user as $course => $lessons) {
-				foreach ($lessons as $lesson ) {
-						// if the user has not complted the previous exit
-						if( !WooThemes_Sensei_Utils::user_completed_lesson( $lesson['pre_requisite_id'] , $user_id ) ){
-							// remove the lesson as the user has not completed the pre-requisite
-							unset( $lesson );
-						}
-				}	
-			}		
-		}	
-
-		return $dynamic_users_courses_lessons; 
-	*/	
+		return $dynamic_users_lessons_courses_dripping_today; 	
 	} // end get_dynamic_user_lessons_list
 
 
@@ -325,5 +309,160 @@ class Scd_Ext_drip_email {
 
 		return $new_users_courses_lessons;
 	}// end get_users_by_courses
+
+	/**
+	* loop through each users lessons and determine if they're dripping today.
+	*
+	* @param array users_courses_lessons
+	* @return array $new_users_courses_lessons 
+	*/
+	function get_dripping_today( $users_courses_lessons ){
+
+		$new_users_courses_lessons = array();
+
+		foreach( $users_courses_lessons as $user ) {			
+			// setup an new user to be used if the lesson is dripping
+			// the purpost for doing it this way is to avoice user and course duplication 
+			// in the return array
+			$new_user = array();
+			$new_user['user_id'] = $user['user_id'];
+
+			foreach( $user['courses'] as $course ) {
+				// setup the course to be used in ther are lessons dripping today
+				$new_course = array();
+				$new_course['course_id'] = $course['course_id'];
+
+				foreach ($course['lessons'] as $lesson ) {
+					// if the lesson is dripping today add the details to 
+					if( $this->is_dripping_today( $lesson['lesson_id'] , $user['user_id'] ) ){
+						$new_course['lessons'][] =  $lesson;
+					}// end if
+
+				}// end for each Lesson
+				
+				// if the inner loop has lessons dripping add this new course to the new user
+				if( !empty( $new_course['lessons'] ) ){
+					$new_user['courses'][] = $new_course;
+				}	
+
+			} // end for each Course
+
+			// if the user has courses with lesson created in the inner 
+			// loops add this user to the returning array
+			if( !empty( $new_user['courses']  ) ){
+				$new_users_courses_lessons[] = $new_user;
+			}
+
+		} // end for each User
+	    
+		return $new_users_courses_lessons;
+	}// end get_dripping_today
+
+	/**
+	* is_dripping_tody. determine if the lesson is dripping today
+	*
+	* @param string $lesson_id 
+	* @return bool  dripping_today
+	*/
+	function is_dripping_today( $lesson_id , $user_id){
+
+		// setup variables needed 
+		$dripping_today = false;
+		$today = new DateTime( date('Y-m-d') ); // get the date ignoring H:M:S
+		
+		// get the lesson drip date
+		$lesson_drip_date = $this->get_lesson_drip_date( $lesson_id , $user_id);
+		
+		// if no lesson drip date could be found exit
+		if( !$lesson_drip_date ){
+			return false;
+		}
+
+		// compare the lesson date with today
+		$offset = $today->diff( $lesson_drip_date );
+		
+		// check if today == $lesson_drip_date
+		// the moment we pickup its not dripping avoid checking the rest
+		// of the values
+		$dripping_today_flag = true;
+
+		foreach ($offset as $key => $value) {
+
+			if( !$dripping_today_flag ){
+				// do not exutuing anything else as this lesson 
+				// has already been defined as not dripping based on 
+				// a previous time interfa unit ( y, m , d )
+				continue;
+			}
+
+			if( 'y' == $key || 'm'  == $key  || 'd'  == $key){
+				if(  0 != $value ){
+					$dripping_today_flag = false;
+				}
+			}
+		} // end for each
+
+		// if the flag was not triggered to be false 
+		if( $dripping_today_flag ){
+			$dripping_today = true;
+		}
+		
+		return $dripping_today;
+	}// end is_dripping_tody
+
+	/**
+	* get_lesson_drip_date. determine the drip type and return the date teh lesson will become available
+	*
+	* @param string $lesson_id 
+	* @param string $user_id
+	* @return DateTime  drip_date format yyyy-mm-dd
+	*/
+	public function get_lesson_drip_date( $lesson_id , $user_id ){
+		//setup the basics, drip date default return will be false on error
+		$drip_date = false;
+
+		if( empty( $lesson_id ) || empty( $user_id ) ){
+			return $drip_date;
+		}
+
+		//get the post meta drip type
+		$drip_type = get_post_meta( $lesson_id , '_sensei_content_drip_type', true );
+
+		if( 'absolute' === $drip_type ){
+
+			$drip_date = new DateTime( $dripped_data['_sensei_content_drip_details_date'] );
+
+		}elseif( 'dynamic' === $drip_type  ){
+			// get the lessons data
+			$dripped_data =  Sensei_Content_Drip()->lesson_admin->get_lesson_drip_data( $lesson_id );
+
+			// get the drip details array data
+			$unit_type  =  get_post_meta( $lesson_id , '_sensei_content_drip_details_date_unit_type', true );  
+			$unit_amount = get_post_meta( $lesson_id , '_sensei_content_drip_details_date_unit_amount', true );
+			$drip_pre_lesson_id = get_post_meta( $lesson_id , '_sensei_content_drip_dynamic_pre_lesson_id', true ); 
+			
+			// get the previous lessons completion date
+			$activitiy_query = array( 'post_id' => $drip_pre_lesson_id, 'user_id' => $user_id, 'type' => 'sensei_lesson_end', 'field' => 'comment_date_gmt' );
+			$user_lesson_end_date_gmt =  WooThemes_Sensei_Utils::sensei_get_activity_value( $activitiy_query  );
+
+			// check if the user has finished the previous course
+			if( !$user_lesson_end_date_gmt  ){
+				return false;
+			}
+
+			// create a date interval object to determine when the lesson should become available
+			$unit_type_first_letter_uppercase = strtoupper( substr( $unit_type, 0, 1 ) ) ; 
+			$interval_to_lesson_availablilty = new DateInterval( 'P'.$unit_amount.$unit_type_first_letter_uppercase );
+
+			// create an object which the interval will be added to and add the interval
+			$lesson_becomes_available_date = new DateTime( $user_lesson_end_date_gmt );
+
+			$drip_date = $lesson_becomes_available_date->add( $interval_to_lesson_availablilty );
+
+		}// end if
+
+		//strip out the hours minutes and seccond before returning the yyyy-mm-dd format
+		return  new DateTime( $drip_date->format('Y-m-d') );
+	}// end get_lesson_drip_date()
 
 }// end Scd_Ext_drip_email
