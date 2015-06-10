91f44234ab6cc0d06aa51196a615d516ab3857ef
Dwain Maralack <dwain.maralack@gmail.com>
2015-06-10 09:57:42 +0200

    add tests for the Scd_Ext_Lesson_Admin::get_all_dripping_lessons() function

--- tests/test-drip-email.php
+++ tests/test-drip-email.php
@@ -4,9 +4,9 @@
 * @class Scd_Ext_Lesson_Admin
 * @file ../includes/class-scd-ext-lesson-admin.php
 */
-class LessonAdminTest extends WP_UnitTestCase {
+class DripEmailTest extends WP_UnitTestCase {
 	function testClassSetup() {
-		$this->assertTrue( class_exists( 'Scd_Ext_Lesson_Admin' ) , 'The plugin class file was not loaded' );
+		$this->assertTrue( class_exists( 'Scd_Ext_drip_email' ) , 'The plugin class file was not loaded' );
 		$this->assertTrue( isset( Sensei_Content_Drip()->drip_email )   , 'The plugin class was not intstantiated' );
 	}
 }
---
+++ tests/test-lesson-admin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* Testing the Sensei Content Drip ( scd ) Email functionality class
+* @class Scd_Ext_lesson_admin
+* @file ../includes/class-scd-ext-drip-email.php
+*/
+class LessonAdminTest extends WP_UnitTestCase {
+
+	function testClass() {
+		$this->assertTrue( class_exists( 'Scd_Ext_Lesson_Admin' ) , 'The plugin class file was not loaded' );
+		$this->assertTrue( isset( Sensei_Content_Drip()->lesson_admin )   , 'The plugin class was not intstantiated' );
+	}
+
+    function testGetAllDrippingLessons(){
+
+        // setup test lessons
+        $lessons = array();
+        $lessons[0] = $this->factory->post->create(array( 'post_type' => 'lesson' ));
+        add_post_meta( $lessons[0], '_sensei_content_drip_type', 'absolute' );
+        $lessons[1] = $this->factory->post->create(array( 'post_type' => 'lesson' ));
+        add_post_meta( $lessons[1], '_sensei_content_drip_type', 'absolute' );
+        $lessons[2] = $this->factory->post->create(array( 'post_type' => 'lesson' ));
+        add_post_meta( $lessons[2], '_sensei_content_drip_type', 'dynamic' );
+        $lessons[3] = $this->factory->post->create(array( 'post_type' => 'lesson' ));
+        add_post_meta( $lessons[3], '_sensei_content_drip_type', 'dynamic' );
+
+        // function call
+        $dripping_lessons = Scd_Ext_Lesson_Admin::get_all_dripping_lessons();
+
+        // assert
+        $this->assertSame( $lessons ,$dripping_lessons  );
+
+    }// end testGetAllDrippingLessons
+}
