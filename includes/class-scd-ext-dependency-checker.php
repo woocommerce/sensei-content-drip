d0234aa8ec5e5bb417839290333598005d69d0b2
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-03-19 12:31:38 +0000

    Change PHP version notice message

--- includes/class-scd-ext-dependency-checker.php
+++ includes/class-scd-ext-dependency-checker.php
@@ -88,7 +88,7 @@ class Scd_Ext_Dependency_Checker {
 		}
 
 		// translators: %1$s is version of PHP that SCD requires; %2$s is the version of PHP WordPress is running on.
-		$message = sprintf( __( '<strong>Sensei Content Drip</strong> requires PHP version %1$s but you are running %2$s.', 'sensei-content-drip' ), self::MINIMUM_PHP_VERSION, phpversion() );
+		$message = sprintf( __( '<strong>Sensei Content Drip</strong> requires a minimum PHP version of %1$s, but you are running %2$s.', 'sensei-content-drip' ), self::MINIMUM_PHP_VERSION, phpversion() );
 		echo '<div class="error"><p>';
 		echo wp_kses( $message, array( 'strong' => array() ) );
 		$php_update_url = 'https://wordpress.org/support/update-php/';
