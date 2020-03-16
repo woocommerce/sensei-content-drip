13ff1d72bdfdb681f190eda0e5ee1574f3c61c61
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-03-16 16:22:52 +0200

    Send email to enroled users only

--- includes/class-scd-ext-drip-email.php
+++ includes/class-scd-ext-drip-email.php
@@ -150,7 +150,11 @@ class Scd_Ext_Drip_Email {
 			}
 
 			// Get all users in this course id.
-			$course_users = Sensei_Content_Drip()->utils->get_course_users( $course_id );
+			if ( Sensei_Content_Drip::instance()->is_legancy_enrolment() ) {
+				$course_users = Sensei_Content_Drip()->utils->get_course_users( $course_id );
+			} else {
+				$course_users = Sensei_Course_Enrolment::get_course_instance( $course_id )->get_enrolled_user_ids();
+			}
 
 			if ( ! empty( $course_users ) ) {
 				// Loop through each of the users for this course and append the lesson id to the user.
