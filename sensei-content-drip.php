8a4875af76b1347fbe3c2b6ef5b04ea7d3c79563
Mikey Arce <mikeyarce@gmail.com>
2022-09-04 16:11:54 -0700

    add a check to see if plugin is already active with Sensei Pro

--- sensei-content-drip.php
+++ sensei-content-drip.php
@@ -21,19 +21,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SENSEI_CONTENT_DRIP_VERSION', '2.1.1' );
-define( 'SENSEI_CONTENT_DRIP_PLUGIN_FILE', __FILE__ );
-define( 'SENSEI_CONTENT_DRIP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-
-require_once dirname( __FILE__ ) . '/includes/class-scd-ext-dependency-checker.php';
-
-if ( ! Scd_Ext_Dependency_Checker::are_system_dependencies_met() ) {
-	return;
+function sensei_content_drip_requirements_met() {
+	if ( defined( 'SENSEI_CONTENT_DRIP_PLUGIN_FILE' ) ) {
+		return false;
+	}
+	return true;
 }
 
-require_once dirname( __FILE__ ) . '/includes/class-sensei-content-drip.php';
+if ( sensei_content_drip_requirements_met() ) {
+	define( 'SENSEI_CONTENT_DRIP_VERSION', '2.1.1' );
+	define( 'SENSEI_CONTENT_DRIP_PLUGIN_FILE', __FILE__ );
+	define( 'SENSEI_CONTENT_DRIP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-// Load the plugin after all the other plugins have loaded.
-add_action( 'plugins_loaded', array( 'Sensei_Content_Drip', 'init' ), 5 ) ;
+	require_once dirname( __FILE__ ) . '/includes/class-scd-ext-dependency-checker.php';
 
-Sensei_Content_Drip::instance();
+	if ( ! Scd_Ext_Dependency_Checker::are_system_dependencies_met() ) {
+		return;
+	}
+
+	require_once dirname( __FILE__ ) . '/includes/class-sensei-content-drip.php';
+
+	// Load the plugin after all the other plugins have loaded.
+	add_action( 'plugins_loaded', array( 'Sensei_Content_Drip', 'init' ), 5 ) ;
+
+	Sensei_Content_Drip::instance();
+}
